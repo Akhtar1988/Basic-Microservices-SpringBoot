aae128465d5ab869e58a90b5d82ce92d3c127af3
Akhtar ali <akthar.ali@embitel.com>
2021-10-02 17:28:30 +0530

    first commit.

---
+++ basic-microservices/src/main/java/com/first/microservices/basicmicroservices/BasicMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.first.microservices.basicmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicMicroservicesApplication.class, args);
+	}
+
+}
