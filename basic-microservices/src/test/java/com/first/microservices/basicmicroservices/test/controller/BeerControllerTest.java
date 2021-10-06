233eb6cc152afd619c60e452f63119f42429a00c
Akhtar ali <akthar.ali@embitel.com>
2021-10-06 19:56:09 +0530

    Added circleci

--- basic-microservices/src/test/java/com/first/microservices/basicmicroservices/test/controller/BeerControllerTest.java
+++ basic-microservices/src/test/java/com/first/microservices/basicmicroservices/test/controller/BeerControllerTest.java
@@ -1,16 +1,30 @@
 package com.first.microservices.basicmicroservices.test.controller;
 
 import com.first.microservices.basicmicroservices.controller.BeerController;
+import com.first.microservices.basicmicroservices.model.BeerDto;
+import com.first.microservices.basicmicroservices.model.BeerStyleEnum;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.math.BigDecimal;
 
 @WebMvcTest(BeerController.class)
 public class BeerControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-    void getBeerById(){
+    void getBeerById() {
 
+    }
+
+    BeerDto getValidBeerDto() {
+        return BeerDto.builder()
+                .beerName("test")
+                .beerStyle(BeerStyleEnum.ALE)
+                .price(new BigDecimal("2.4"))
+                .upc(121234567L)
+                .build();
     }
 }
