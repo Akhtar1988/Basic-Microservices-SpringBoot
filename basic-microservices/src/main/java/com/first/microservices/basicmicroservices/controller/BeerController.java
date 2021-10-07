58026028179e070f46adb0615b72474d48f73344
Akhtar ali <akthar.ali@embitel.com>
2021-10-07 23:14:32 +0530

    added jacson config for BeerDto

--- basic-microservices/src/main/java/com/first/microservices/basicmicroservices/controller/BeerController.java
+++ basic-microservices/src/main/java/com/first/microservices/basicmicroservices/controller/BeerController.java
@@ -1,6 +1,9 @@
 package com.first.microservices.basicmicroservices.controller;
 
+import com.first.microservices.basicmicroservices.mappers.BeerMapper;
 import com.first.microservices.basicmicroservices.model.BeerDto;
+import com.first.microservices.basicmicroservices.repositories.BeerRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -8,25 +11,35 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/beer")
 @RestController
 public class BeerController {
 
+    private final BeerMapper beerMapper;
+    private final BeerRepository beerRepository;
+
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
         //todo Impl
-        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+        return new ResponseEntity<>(beerMapper.beerToBeerDto(beerRepository.findById(beerId).get()), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity saveNewBeer(@RequestBody @Validated BeerDto beerDto) {
-        //todo Impl
+        beerRepository.save(beerMapper.beerDtoToBeer(beerDto));
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
     public ResponseEntity updateBeerId(@PathVariable("beerId") UUID beerId, @RequestBody @Validated BeerDto beerDto) {
-        //todo Impl
+
+        beerRepository.findById(beerId).ifPresent(beer -> {
+            beer.setBeerName(beerDto.getBeerName());
+            beer.setBeerStyle(beerDto.getBeerStyle().name());
+            beer.setPrice(beerDto.getPrice());
+            beerRepository.save(beer);
+        });
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
--- basic-microservices/src/main/java/com/first/microservices/basicmicroservices/model/BeerDto.java
+++ basic-microservices/src/main/java/com/first/microservices/basicmicroservices/model/BeerDto.java
@@ -1,14 +1,12 @@
 package com.first.microservices.basicmicroservices.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
-import javax.validation.constraints.Positive;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -25,12 +23,15 @@ public class BeerDto {
     private  Integer version;
 
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd 'T' HH:mm:ssZ",shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createDate;
 
     @Null
+    @JsonFormat(pattern = "yyyy-MM-dd 'T' HH:mm:ssZ",shape = JsonFormat.Shape.STRING)
     private OffsetDateTime lastModifiedDate;
 
     @NotBlank
+    @Size(min = 3 , max = 15)
     private String beerName;
 
     @NotNull
@@ -40,10 +41,12 @@ public class BeerDto {
     @NotNull
     private  Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     @Positive
     @NotNull
     private BigDecimal price;
 
+    @Positive
     private Integer quantityOnHand;
 
 }
